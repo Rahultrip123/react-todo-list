e10bd7a1db7cf02c3443ecbefe52b6833abe5bdf
Rahultrip123 <rahultripathi0080@gmail.com>
2023-09-16 17:45:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Create from "./Components/Create";
+import Read from "./Components/Read";
+import Update from "./Components/Update";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter basename="/react-todo-list">
+        <Routes>
+          <Route exact path="/react-todo-list" element={<Create />}></Route>
+          <Route exact path="/read" element={<Read />}></Route>
+          <Route exact path="/update" element={<Update />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
